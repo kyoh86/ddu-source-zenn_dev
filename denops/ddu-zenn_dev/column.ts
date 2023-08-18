3027223d577b3600cca24c2060382ce77e4bccb5
kyoh86 <me@kyoh86.dev>
2023-08-19 01:10:43 +0900

    update dependencies

--- denops/ddu-zenn_dev/column.ts
+++ denops/ddu-zenn_dev/column.ts
@@ -1,9 +1,9 @@
 import {
   BaseColumn,
   DduItem,
-} from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
-import { GetTextResult } from "https://deno.land/x/ddu_vim@v3.4.4/base/column.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.4.4/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
+import { GetTextResult } from "https://deno.land/x/ddu_vim@v3.5.1/base/column.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.5.1/deps.ts";
 import { ActionData } from "./types.ts";
 
 type Params = {
