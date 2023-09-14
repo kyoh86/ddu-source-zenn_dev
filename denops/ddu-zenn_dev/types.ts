c189bc532162cd41518c1a8bd5b893884492839c
GitHub <noreply@github.com>
2023-09-14 09:01:55 +0000

    chore(deps): Update deno dependencies

--- denops/ddu-zenn_dev/types.ts
+++ denops/ddu-zenn_dev/types.ts
@@ -1,4 +1,4 @@
-import { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+import { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.0/file.ts";
 
 export type Frontmatter = { title: string; emoji: string };
 export type PostAttribute = { slug: string } & Frontmatter;
