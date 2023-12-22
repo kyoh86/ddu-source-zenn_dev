a6f36778209531c8d8d3a3046a8157ef47873e83
GitHub <noreply@github.com>
2023-12-22 09:00:52 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/zenn_dev_article.ts
+++ denops/@ddu-sources/zenn_dev_article.ts
@@ -1,10 +1,10 @@
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/source.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
 import { fn } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
-import { join } from "https://deno.land/std@0.209.0/path/mod.ts";
-import { basename } from "https://deno.land/std@0.209.0/path/mod.ts";
-import { extract } from "https://deno.land/std@0.209.0/front_matter/any.ts";
-import { abortable } from "https://deno.land/std@0.209.0/async/mod.ts";
+import { join } from "https://deno.land/std@0.210.0/path/mod.ts";
+import { basename } from "https://deno.land/std@0.210.0/path/mod.ts";
+import { extract } from "https://deno.land/std@0.210.0/front_matter/any.ts";
+import { abortable } from "https://deno.land/std@0.210.0/async/mod.ts";
 import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.8.1/utils.ts";
 import { ActionData, Frontmatter } from "../ddu-zenn_dev/types.ts";
 
