90e944c688d62cb053604e78d31048163d7e8066
kyoh86 <me@kyoh86.dev>
2024-02-29 22:44:44 +0900

    Update dependencies

--- denops/@ddu-sources/zenn_dev_article.ts
+++ denops/@ddu-sources/zenn_dev_article.ts
@@ -1,10 +1,10 @@
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/source.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 import { fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
-import { join } from "https://deno.land/std@0.216.0/path/mod.ts";
-import { basename } from "https://deno.land/std@0.216.0/path/mod.ts";
-import { extract } from "https://deno.land/std@0.216.0/front_matter/any.ts";
-import { abortable } from "https://deno.land/std@0.216.0/async/mod.ts";
+import { join } from "https://deno.land/std@0.217.0/path/mod.ts";
+import { basename } from "https://deno.land/std@0.217.0/path/mod.ts";
+import { extract } from "https://deno.land/std@0.217.0/front_matter/any.ts";
+import { abortable } from "https://deno.land/std@0.217.0/async/mod.ts";
 import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.10.2/utils.ts";
 import { ActionData, Frontmatter } from "../ddu-zenn_dev/types.ts";
 
