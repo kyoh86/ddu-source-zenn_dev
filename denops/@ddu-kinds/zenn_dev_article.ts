83b3ae8be8bbcb6057af770b507d3a018c20a8fc
GitHub <noreply@github.com>
2024-04-06 09:01:12 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-kinds/zenn_dev_article.ts
+++ denops/@ddu-kinds/zenn_dev_article.ts
@@ -8,7 +8,7 @@ import {
   ensure,
   is,
   maybe,
-} from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
+} from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
 
 type Params = {
   trashCommand: string[];
